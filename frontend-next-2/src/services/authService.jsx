7a3260009a9fb278fc8ca64fcf46d4645688edc1
Pranavlovescode <pranavtitambe04@gmail.com>
2025-04-14 21:07:47 +0530

    updated the domains

--- frontend-next-2/src/services/authService.jsx
+++ frontend-next-2/src/services/authService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://skillsync-angular-backend.pranavtitambe.in';
 
 export const authService = {
   login: async (email, password) => {
