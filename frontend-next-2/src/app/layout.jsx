7e902e2ade471e7d6d226ef3569160a09e4a99c1
Pranavlovescode <pranavtitambe04@gmail.com>
2025-04-17 19:43:40 +0530

    added the fix

--- frontend-next-2/src/app/layout.jsx
+++ frontend-next-2/src/app/layout.jsx
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-MF2RFZZ8"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
         {children}
